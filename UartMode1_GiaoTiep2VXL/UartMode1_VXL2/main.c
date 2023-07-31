45f21bc268a3971b8a6a646c976e415de4d4f03a
Hung <hunghungyen30803@gmail.com>
2023-07-31 09:32:53 +0700

    first commit

---
+++ UartMode1_GiaoTiep2VXL/UartMode1_VXL2/main.c
@@ -0,0 +1,24 @@
+#include<main.h>
+#include<..\lib\UartMode1.h"
+
+void main()
+{
+	P1_0 = 0;		// Tat LED
+
+	Uart_Init();
+
+	while(1)
+	{
+		if(Uart_Data_Ready())
+		{
+		 	if(Uart_Read()==0x01)
+			{
+			 	P1_0 = 1;	// Bat LED
+			}
+			else
+			{
+			 	P1_0 = 0;	// Tat LED
+			}
+		}
+	}
+}
